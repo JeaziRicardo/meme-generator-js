160925df443b53f425125b3d6376ebc3f5abba3f
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-02-23 20:54:31 -0300

    Adiciona três botões, que ao serem clicados cada um troca a própria borda ao redor do container

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const memeContainer = document.querySelector('#meme-image-container');
 const memeText = document.querySelector('#meme-text');
 const memeImage = document.querySelector('#meme-image');
 const file = document.querySelector('#meme-insert');
+const fireBtn = document.querySelector('#fire');
+const waterBtn = document.querySelector('#water');
+const earthBtn = document.querySelector('#earth');
+
 
 function textInContainer() {
   memeText.innerHTML = '';
@@ -15,3 +19,18 @@ function imageUp() {
   memeImage.src = URL.createObjectURL(file.files[0]);
 }
 file.addEventListener('change', imageUp);
+
+function changeBorderFire() {
+  memeContainer.style.border = 'dashed 3px red';
+}
+fireBtn.addEventListener('click', changeBorderFire);
+
+function changeBorderWater() {
+  memeContainer.style.border = 'double 5px blue';
+}
+waterBtn.addEventListener('click', changeBorderWater);
+
+function changeBorderEarth() {
+  memeContainer.style.border = 'groove 6px green';
+}
+earthBtn.addEventListener('click', changeBorderEarth);
